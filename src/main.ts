1530d3b03f23ab622d37f3fcdb078b6f4408d641
seniorsardorbek <sardorbekmusilman@gmail.com>
2024-01-23 10:45:44 +0500

    last

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule, {cors: false});
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     })
   )
-  await app.listen(5000);
+  await app.listen(3000);
 }
 bootstrap();
