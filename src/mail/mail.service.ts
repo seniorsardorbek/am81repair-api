0f8c17cf45bd8ec43c8f9877e44e3b85635754d8
seniorsardorbek <sardorbekmusilman@gmail.com>
2024-01-23 12:18:39 +0500

    djjd

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -11,8 +11,12 @@ export class MailService {
       port: 587,
       secure: false,
       auth: {
+        type: "OAuth2",
         user: "amrepair81@gmail.com",
-        pass: "arfm bvwk kpdc msfc",
+        clientId: "612540618943-643qkkcmo1cu0vdt0id3ngk0gsnenrtn.apps.googleusercontent.com",
+        clientSecret: "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",
+        refreshToken: "1//041QS4RFwelVlCgYIARAAGAQSNwF-L9Ir1JyWQ1-wd1ym-bJncZOJBd_o6ftFwA2O2QQSBTMhU0qutQ-PTo7IREj25VP2jBKzmfk",
+        accessToken: "ya29.a0AfB_byBMELiZTc8J89726RNs4TrY6RGZXAiHZG0Ipid0EtSsBfenucTlmTrpc88A_fYTphuzRE6qOFfENg4a-IuNohwpu4BvpFbrcIHygN2PxtAjTOTarW4DfG-rDaU4hiHDuzVDd-1Uzo4tc-PLS4qsw0dmfJLwnwwSaCgYKAZMSARISFQHGX2MirhNaEYyKK8_aM7bY6Xuu9g0171",
       },
     });
   }
